1287f2a624257798164a8e8f4ffb34c2531d6308
Meenakshi-710 <meenakshi@tequity.tech>
2025-07-30 18:57:20 +0530

    Calendar synced improved

--- src/lib/googleAuth.ts
+++ src/lib/googleAuth.ts
@@ -6,7 +6,7 @@ const isChromeExtension = typeof chrome !== "undefined" && chrome.identity;
 // Chrome extensions must use this format as redirect URI
 const REDIRECT_URI = isChromeExtension
   ? chrome.identity.getRedirectURL("oauth2")
-  : window.location.origin;
+  :"http://localhost:5173";
 
 console.log("OAuth Redirect URI:", REDIRECT_URI);
 
